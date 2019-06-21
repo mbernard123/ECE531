1cec7369837f033f4e7227a0aabc4fa7fab6a44b
Marc Bernard <mbernard@truno.com>
2019-06-21 14:36:02 -0600

    first code changes

--- makefile
+++ makefile
@@ -1,1 +1,24 @@
+# cc=arm-linux-gnueabi-gcc
+CC=gcc
+CCFLAGS=
+INCLUDES=
+LFLAGS=-L/usr/lib/x86_64-linux-gnu
+LIBS=-lcurl -lpthread
+
+SRC=requestor.c
+OBJ=$(SRC:.c=.o)
+MAIN=test
+
+RM=rm -rf
+
+.c.o:
+	$(CC) $(CFLAGS) $(INCLUDED) -c $< -o $@
+
+$(MAIN): $(OBJ)
+	$(CC) $(CCFLAGS) $(INCLUDES) -o $(MAIN) $(OBJ) $(LFLAGS) $(LIBS)
+
+all: $(MAIN)
+
+clean:
+	$(RM) $(MAIN) *.o *~
 
--- requestor.c
+++ requestor.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <curl/curl.h>
 
+#define OK        0
+#define INIT_ERR  1
+#define REQ_ERR   2
+
+#define URL       "http://localhost:8000"
+
+int main(void)
+{
+ CURL* curl;
+ CURLcode res;
+
+ curl = curl_easy_init();
+ if (curl)
+ {
+  curl_easy_setopt(curl, CURLOPT_URL, URL);
+  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)l
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK) 
+  {
+   return REQ_ERR;
+  }
+  curl_easy_cleanup(curl);
+ } // Endif (curl)
+ else
+ {
+  return INIT_ERR;
+ } // Endif (curl_easy_init failed) 
+
+ return OK;
+} // End Function main() 
